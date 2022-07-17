e42e82925a9bf1ce965a640b7142e0d30d3c9e33
Alyssa Tadeo <alyssajtadeo@gmail.com>
2022-07-16 23:28:25 -0700

    Host on local wifi

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@
 var http = require("http");
 
 /* Create an HTTP server to handle responses */
+const hostname = '192.168.1.31';
+const port = 8888;
 
 http.createServer(function(request, response) {
 response.writeHead(200, {"Content-Type": "text/plain"});
 response.write("Hello World");
 response.end();
-}).listen(8888, '0.0.0.0');
+}).listen(port, hostname);
