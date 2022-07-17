75fd722424f61827a02bda61b0f123f516f31942
Alyssa Tadeo <alyssajtadeo@gmail.com>
2022-07-16 20:04:20 -0700

    Add test images and reorganize code

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+import config.py # contains spotify client ID, client secret keys
+
+scope = "user-read-currently-playing"
+
+client_credentials_manager=SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(scope=scope), client_credentials_manager=client_credentials_manager)
+
