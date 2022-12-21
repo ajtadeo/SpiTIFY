a717574c0804fab26a8aa37aa8213dc5b6dd59b3
Alyssa Tadeo <alyssajtadeo@gmail.com>
2022-12-20 22:57:38 -0800

    view organization and rendering methods

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-const { assert } = require("console");
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const dotenv = require("dotenv")
-var cookieParser = require('cookie-parser');
-var request = require('request');
-const { access } = require("fs");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { getEnv } from "./static/utils.js";
 
-// https://stackoverflow.com/questions/68329418/in-javascript-how-can-i-throw-an-error-if-an-environmental-variable-is-missing
-function getEnv(name){
-    let val = process.env[name];
-    if ((val === undefined) || (val === null)) {
-        throw ("Spotipi: Error: Missing "+ name +" in ./.env");
-    }
-    return val;
-}
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-/* dotenv setup */
+var loggedIn = false;
+
+/* dotenv Setup */
 const result = dotenv.config();
 if (result.error){
     console.log("Spotipi: Error: dotenv not configured correctly.")
@@ -25,128 +20,28 @@ if (result.error){
 
 const port = getEnv("PORT");
 const hostname = getEnv("HOSTNAME");
-const client_id = getEnv("SPOTIFY_CLIENT_ID");
-const redirect_uri = getEnv("SPOTIFY_REDIRECT_URI");
-const client_secret = getEnv("SPOTIFY_CLIENT_SECRET");
 
-/* Express.js and socket.io setup */
+/* Express.js and socket.io Setup */
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-/* Cookie methods */
-var generateRandomString = function (length) {
-    var text = '';
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-    for (var i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-};
-var stateKey = 'spotify_auth_state';
-
-/* Routing methods */
-app.use(cookieParser());
+/* Rendering Methods */
+app.use("/static", express.static("./static/"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-});
-
-// request authorization from Spotify
-app.get("/login", (req, res) => {
-    var state = generateRandomString(16);
-    res.cookie(stateKey, state);
-    var scope = "user-read-currently-playing";
-    var params = new URLSearchParams([
-        ['response_type', 'code'],
-        ['client_id', client_id],
-        ['scope', scope],
-        ['redirect_uri', redirect_uri],
-        ['state', state]
-    ]);
-    res.redirect("https://accounts.spotify.com/authorize?" + params.toString());
-});
-
-// Spotify authorization callback
-app.get("/callback", (req, res) => {
-    var code = req.query.code || null;
-    var state = req.query.state || null;
-    var storedState = req.cookies ? req.cookies[stateKey] : null;
-    if (state == null || state !== storedState) {
-        var params = new URLSearchParams({ 'error': 'state_mismatch' });
-        res.redirect('/#' + params.toString());
+    var view;
+    if (loggedIn == false){
+        view = "/public/views/Login.html";
     } else {
-        // get api token
-        res.clearCookie(stateKey);
-        var authOptions = {
-            url: 'https://accounts.spotify.com/api/token',
-            form: {
-                code: code,
-                redirect_uri: redirect_uri,
-                grant_type: 'authorization_code'
-            },
-            headers: {
-                'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64')
-            },
-            json: true
-        };
-
-        // post response
-        var status = "";
-        request.post(authOptions, (error, response, body) => {
-            if (!error && response.statusCode === 200) {
-                var access_token = body.access_token;
-                var refresh_token = body.refresh_token;
-                var options = {
-                    url: 'https://api.spotify.com/v1/me',
-                    headers: { 'Authorization': 'Bearer ' + access_token },
-                    json: true
-                };
-
-                request.get(options, (error, response, body) => {
-                    console.log(body);
-                });
-
-                // pass token to the browser
-                var params = new URLSearchParams([
-                    ["access_token", access_token],
-                    ["refresh_token", refresh_token]
-                ]);
-                status = "Authorized"
-                res.redirect('/#' + params.toString());
-            } else {
-                var params = new URLSearchParams({ "error": "invalid_token" });
-                status = "Invalid Token"
-                res.redirect('/#', params.toString());
-            }
-        });
+        view = "/public/views/Home.html";
     }
+    res.sendFile(__dirname + view);
 });
 
-// refresh access token
-app.get("/refresh_token", (req, res) => {
-    var refresh_token = req.query.refresh_token;
-    var authOptions = {
-        url: 'https://accounts.spotify.com/api/token',
-        headers: { 'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64') },
-        form: {
-            grant_type: 'refresh_token',
-            refresh_token: refresh_token
-        },
-        json: true
-    };
-
-    request.post(authOptions, (error, response, body) =>{
-        if (!error && response.statusCode == 200){
-            var access_token = body.access_token;
-            res.send({
-                "access_token": access_token
-            });
-        }
-    });
-});
-
+app.get("*", (req, res) => {
+    res.status(404).sendFile(__dirname + "/public/views/NotFound.html");
+})
 
 /* Server connection */
 io.on("connection", (socket) => {
